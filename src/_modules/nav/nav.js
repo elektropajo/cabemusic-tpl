911430abd5d886db44889716e2201cf16c6aadf7
Andrés Bayón <andres.bayon@riseup.net>
2015-09-03 12:24:48 +0200

    Navigator initial conf. Hexa correction.

--- src/_modules/nav/nav.js
+++ src/_modules/nav/nav.js
@@ -6,17 +6,44 @@ export default class Nav {
   }
   listen() {
     console.log('Nav listening');
-    // Level listeners
+    this.toggle();
+    this.watchSize();
+  }
+  toggle () {
     let callers = document.querySelectorAll('[data-togglelevel]');
     for (let i=0; i < callers.length; i++) {
       callers[i].addEventListener('click', event => {
         let level = event.target.dataset.togglelevel,
             target = event.target.parentNode;
-        if (target.dataset.togglelevel !== level) {
+        console.log('Current level: '+ level + '\nTarget: '+ target.dataset.level);
+        if (target.dataset.level !== level) {
           target = target.querySelector('[data-level]');
         }
         target.classList.toggle('is-submenu-open', !target.classList.contains('is-submenu-open'));
       });
+    }
+  }
+  watchSize() {
+    this.position();
+    window.addEventListener('resize', event => {
+      this.position();
+    });
+  }
+  position() {
+    let w = window.innerWidth,
+        colap = 16 * 62, //16px*62em
+        caller = document.querySelector('[data-togglelevel]'),
+        submenus = document.querySelectorAll('[data-level]'),
+        right;
+    // TODO: Calcular offsetRight correctamente
+    right = w - caller.offsetLeft - caller.clientWidth - 700;
+    if (w < colap) {
+      right = 'auto';
+    } else if (w > colap) {
+      right = right + 'px';
+    }
+    for (let i=0; i < submenus.length; i++) {
+      submenus[i].style.right = right;
     }
   }
 }
